7a95520f8cfa03a118a321c543de94430cb13c10
Mailson <marq1916@outlook.com>
2025-09-10 20:42:34 -0300

    Subindo hook

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
-import TrabalhandoComImagens from './components/TrabalhandoComImagens'
+import HookContador from './components/HookContador'
 
 const App = (estado) => {
   return (
     <div>
-      <RenderizandoComFuncoes/>
-      <TrabalhandoComImagens/>
+
+      <HookContador/>
     </div>
   )
 }
