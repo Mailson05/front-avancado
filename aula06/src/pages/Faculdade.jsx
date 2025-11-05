ae736fef0761a3f9afedb6a46f40dc6e0bd0c0ad
José Mailson Marques da Silva <2024111510165@academicos.com>
2025-11-05 20:52:29 -0300

    add aula06

---
+++ aula06/src/pages/Faculdade.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Faculdade = () => {
+  return (
+    <div>Essa vai ser a página sobre a Faculdade</div>
+  )
+}
+
+export default Faculdade
---
+++ aula06/src/pages/Noticias.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Noticias = () => {
+  return (
+    <div>Aqui vai ser a página de noticias</div>
+  )
+}
+
+export default Noticias
