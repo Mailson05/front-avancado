c20b418892d4dcb53a4a236db5e9fde2a1f94aac
Mailson <mailsonestudo48@gmail.com>
2025-12-03 12:27:53 -0300

    entregavel final

--- site-uniesp/src/App.jsx
+++ site-uniesp/src/App.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Inicial from './pages/Inicial'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import CustomNavbar from './components/CustomNavBar'
 import Faculdade from './pages/Faculdade'
+import Footer from './components/Footer'
+import Dpo from './pages/Dpo'
+import './App.css'
+import Noticias from './pages/Noticias'
 
 const App = () => {
   return (
-    
+    <div className="layout">
       <BrowserRouter>
-      <CustomNavbar />
+        <CustomNavbar />
+
         <Container className="my-4">
           <Routes>
             <Route path="/" element={<Inicial />} />
             <Route path="/a-faculdade" element={<Faculdade />} />
+            <Route path="/dpo-lgpd" element={<Dpo/>}/>
+            <Route path="/noticias" element={<Noticias/>}/>
           </Routes>
         </Container>
       </BrowserRouter>
-    
+
+      <Footer />
+    </div>
   )
 }
 
